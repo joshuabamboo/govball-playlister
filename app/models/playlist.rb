6eb37d7d5d67247fd7ce72883952d01649567820
joshuabamboo <joshuaowens011@gmail.com>
2016-03-15 23:44:52 -0400

    add service object for spotify

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,20 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
+
+  private
+  def client
+    SpotifyClient.for(current_user)
+  end
 end
+
+
+# p = Playlist.new
+# p.client.create_playlist!(tracks)
+#
+# login
+# click generate Playlist
+# create a playlist of with all artists
+#  - get all artists
+#  - get their top track
+#  - add tracks to a new playlist
+#  - 
