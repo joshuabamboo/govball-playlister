f49b6d0fd5da5b65b027e504c07677eaf96a02b7
joshuabamboo <joshuaowens011@gmail.com>
2016-03-17 18:11:23 -0400

    WIP playlist creation logic

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,27 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
 
+  def generate_top_tracks_playlist
+    playlist = client.create_playlist!("GovBall Top Tracks")
+    playlist.add_tracks!(get_top_tracks)
+  end
+
+  def get_top_tracks_for(festival_name)
+    artists = Artist.all.select do |artist_object|
+      artist_object.festival_days.first.festival.name == festival_name
+    end
+
+    spotify_artists = artists.each do |artist|
+      binding.pry
+      RSpotify::Artist.search(artist.name).first
+    end
+
+    spotify_artists.collect do |artist|
+      binding.pry
+      artist.top_tracks(:US)
+    end
+  end
+
   private
   def client
     SpotifyClient.for(current_user)
@@ -17,4 +38,4 @@ end
 #  - get all artists
 #  - get their top track
 #  - add tracks to a new playlist
-#  - 
+#  -
