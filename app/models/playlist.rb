19968bed54cd17b62994f5968704fe0445aefb7f
joshuabamboo <joshuaowens011@gmail.com>
2016-03-26 23:34:36 -0400

    wip refactor

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -38,22 +38,31 @@ class Playlist < ActiveRecord::Base
     # artists.select do |artist|
     #   spotify_artists << RSpotify::Artist.search(artist.name).first
     # end
-    convert_artists_to_rspotify_objects(artists)
+    spotify_artists = convert_artists_to_rspotify_objects(artists)
 
     # create an array of the top track for a given artist
-    tracks = []
-    spotify_artists.collect do |artist|
-      tracks << artist.top_tracks(:US).first
-    end
-    tracks
+    # tracks = []
+    # spotify_artists.collect do |artist|
+    #   tracks << artist.top_tracks(:US).first
+    # end
+    # tracks
+    get_top_tracks(spotify_artists)
   end
 
+#helpers
+  #Festival class
   def get_lineup(festival_name)
     Artist.all.select do |artist_object|
       artist_object.festival_days.first.festival.name == festival_name
     end
   end
 
+  #Festival class
+  def filter_artists_by_day(artists, day)
+    artists.select {|a| a if a.festival_days.find_by(start_date: day)}
+  end
+
+  #SpotifyClient class?
   def convert_artists_to_rspotify_objects(artists)
     spotify_artists = []
     artists.select do |artist|
@@ -62,9 +71,14 @@ class Playlist < ActiveRecord::Base
     spotify_artists
   end
 
-  def filter_artists_by_day(artists, day)
-      artists.select {|a| a if a.festival_days.find_by(start_date: day)}
+  def get_top_tracks(artists)
+    tracks = []
+    spotify_artists.collect do |artist|
+      tracks << artist.top_tracks(:US).first
+    end
+    tracks
   end
+
 
   private
   def client
