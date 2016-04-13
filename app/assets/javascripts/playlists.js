ab747816e090b7adaf1862510b346decdaa1a13e
joshuabamboo <joshuaowens011@gmail.com>
2016-04-13 18:32:08 -0400

    focus cursor on playlist's title input

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,3 +1,7 @@
 $(function(){
+  // dynamically alter the playlist's height
   $('.spotify-iframe').css('height', $(window).height()-200+'px');
+
+  // focus cursor on playlist's title input
+  $('.playlist-title-input').focus();
 });
