40b70f33822f4c62332474187b8ce1bb80460ed2
joshuabamboo <joshuaowens011@gmail.com>
2016-04-28 17:07:37 -0400

    style playlists#index

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,7 +2,8 @@ class PlaylistsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @playlists = Festival.find(params[:festival_id]).playlists
+    @fest = Festival.find(params[:festival_id])
+    @playlists = @fest.playlists
   end
 
   def new
